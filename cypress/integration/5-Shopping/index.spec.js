62c271220322b34bcc4d8011efdd277d98b67908
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2022-03-17 10:38:10 +0100

    add simple shopping cart test

--- cypress/integration/5-Shopping/index.spec.js
+++ cypress/integration/5-Shopping/index.spec.js
@@ -1,22 +1,36 @@
 describe('Search a product to add to cart', () => {
   before(() => {
-    cy.visit('https://www.boutique-bedecine.fr/');
+    cy.visit('https://www.i-run.fr/');
+
+    // cy.get('.didomi-popup-view').should('be.visible');
+    cy.get('#didomi-notice-agree-button').should('be.visible').click();
   });
 
-  it('Search bar is visible', () => {
-    cy.get('#search_simple_term').should('be.visible');
-  });
+  it('Search bar is visible and search a product', () => {
+    cy.get('form#rechercheform input').should('be.visible');
 
-  it('Search a short', () => {
-    const searchTerm = 'Dragon Ball Z';
-    cy.get('#search_simple_term').type(searchTerm)
+    const searchTerm = 'mizuno';
+    cy.get('#rechercheform input').type(searchTerm)
       .should('have.value', searchTerm);
 
-    cy.get('form[name="search_simple"]').submit();
+    cy.get('form#rechercheform').submit();
   });
 
-  it('Select the first reference', () => {
-    cy.get('.features_items')
-      .first('.single-products').click();
+  it('Select the first reference and add to cart', () => {
+    cy.get('#productsContainer .bc_produit').first().click();
+
+    cy.get('#didomi-notice-agree-button').should('be.visible').click();
+    cy.get('#img_principale').should('be.visible');
+
+    cy.get('.ui-selectmenu-status').click();
+
+    cy.get('#s_tailles-menu li').contains(/42/g).click();
+
+    cy.get('#bt_commander').should('be.visible').click();
+  });
+
+  it('Cart contains product', () => {
+    cy.get('.cart__content').should('be.visible');
+    cy.contains('Mizuno').should('be.visible');
   });
 });
